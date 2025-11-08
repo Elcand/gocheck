f0fa73f45f780ea1baeb69e1c8d44439226ec9aa
Mikhael Candra <tjandraraharja99@gmail.com>
2025-11-08 13:26:15 +0700

    show item from data with render list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+const listItems = [
+  {
+    id: 1,
+    title: "Makan",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "Minum",
+    done: false,
+  },
+  {
+    id: 3,
+    title: "Tidur",
+    done: true,
+  },
+  {
+    id: 4,
+    title: "Ngobrol",
+    done: false,
+  },
+  {
+    id: 5,
+    title: "Nonton",
+    done: false,
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Logo />
+      <Form />
+      <Checklist />
+      <Stats />
+    </div>
+  );
 }
 
-export default App
+function Logo() {
+  return <span className="logo">🤞 GoCheck</span>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>Add a ToDo</h3>
+    </div>
+  );
+}
+
+function Checklist() {
+  return (
+    <div className="list">
+      <ul>
+        {listItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+type TodoItem = {
+  // ini untk pengganti 'any' di ts, kalo js ga perlu
+  id: number;
+  title: string;
+  done: boolean;
+};
+
+function Item({ item }: { item: TodoItem }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span style={{ textDecoration: item.done ? "line-through" : "none" }}>
+        {item.title}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <span># You have x notes & only x have been checked off (x%) </span>
+    </footer>
+  );
+}
+
+export default App;
